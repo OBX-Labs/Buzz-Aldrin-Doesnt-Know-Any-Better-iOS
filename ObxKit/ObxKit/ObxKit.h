79c9c30e988a8bf1fa73eeaab0a211857d77875e
Shahrom <mshahromali@gmail.com>
2024-06-06 16:42:11 -0400

    Initializing ObxKit Framework

---
+++ ObxKit/ObxKit/ObxKit.h
@@ -0,0 +1,18 @@
+//
+//  ObxKit.h
+//  ObxKit
+//
+//  Created by Muhammad Shahrom Ali on 2024-06-06.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObxKit.
+FOUNDATION_EXPORT double ObxKitVersionNumber;
+
+//! Project version string for ObxKit.
+FOUNDATION_EXPORT const unsigned char ObxKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObxKit/PublicHeader.h>
+
+
