81c429e5e97bc5377d6308c75809ecf1ae3acc10
Shahrom <mshahromali@gmail.com>
2024-06-10 16:33:43 -0400

    Adding all of Account Manager

---
+++ ObxKit/ObxKit/AccountManager/AccountManagerViews/OKNewAccount.h
@@ -0,0 +1,42 @@
+//
+//  OKNewAccount.h
+//  ObxKit
+//  Created by Muhammad Shahrom Ali on 2024-06-10.
+//  Programmed by Christian Gratton on 2013-02-04.
+//  Copyright (c) 2024 Obx Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OKInfoView.h"
+
+@class OKInfoView;
+@class OKRegistration;
+
+@interface OKNewAccount : UITableViewController <UIAlertViewDelegate, UITextFieldDelegate>
+{
+    // TableView Sections
+    NSMutableArray *sections;
+    
+    // Validation errors
+    NSMutableArray *validationErrors;
+    
+    // First Responders
+    NSMutableArray *firstResponders;
+    
+    // Display ViewController
+    OKInfoView *display;
+    
+    // Account type
+    OKAccountType accountType;
+    
+    // Keyboard switch
+    BOOL keyboardIsShown;
+    
+    // Row Switcher
+    NSIndexPath *sRow;
+}
+
+- (id) initWithTitle:(NSString*)aTitle style:(UITableViewStyle)aStyle forType:(OKAccountType)aType;
+- (void) setDisplayViewController:(OKInfoView*)aDisplay;
+
+@end
---
+++ ObxKit/ObxKit/AccountManager/AccountTypes/OKLimitedEdition.h
@@ -0,0 +1,30 @@
+//
+//  OKLimitedEdition.h
+//  ObxKit
+//  Created by Muhammad Shahrom Ali on 2024-06-10.
+//  Programmed by Christian Gratton on 2013-02-04.
+//  Copyright (c) 2024 Obx Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OKInfoView.h"
+
+@class OKInfoView;
+
+@interface OKLimitedEdition : UITableViewController
+{
+    // TableView Sections
+    NSMutableArray *sections;
+    
+    // Display ViewController
+    OKInfoView *display;
+    
+    // Account type
+    OKAccountType accountType;
+}
+
+- (id) initWithTitle:(NSString*)aTitle style:(UITableViewStyle)aStyle forType:(OKAccountType)aType;
+- (void) setDisplayViewController:(OKInfoView*)aDisplay;
+
+
+@end
